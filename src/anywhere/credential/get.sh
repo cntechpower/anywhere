e71662b8c9ef99bd458ba1283d2628a468093495
dujinyang <root@cntechpower.com>
2019-09-07 20:11:17 +0800

    update credentials

--- src/anywhere/credential/get.sh
+++ src/anywhere/credential/get.sh
@@ -1,10 +1,11 @@
+# 生成CA根证书
 openssl genrsa -out ca.key 2048
 # 使用-x509参数生成证书
 openssl req -new -x509 -days 7200 -key ca.key -out ca.pem
 
 # 生成服务端证书
-openssl genrsa -out server.key 2048
-# openssl ecparam -genkey -name secp384r1 -out server.key
+# openssl genrsa -out server.key 2048
+openssl ecparam -genkey -name secp384r1 -out server.key
 # 生成证书请求文件
 openssl req -new -key server.key -out server.csr
 # 基于CA签发服务端证书
@@ -15,3 +16,4 @@ openssl x509 -req -sha256 -CA ca.pem -CAkey ca.key -CAcreateserial -days 3650 -i
 openssl ecparam -genkey -name secp384r1 -out client.key
 openssl req -new -key client.key -out client.csr
 openssl x509 -req -sha256 -CA ca.pem -CAkey ca.key -CAcreateserial -days 3650 -in client.csr -out client.pem
+
