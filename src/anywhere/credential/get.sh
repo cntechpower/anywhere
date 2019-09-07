7b5e9550fd968c50f1e14fdebda2a04dbb15c2b7
dujinyang <root@cntechpower.com>
2019-09-07 20:22:02 +0800

    update scredentials

--- src/anywhere/credential/get.sh
+++ src/anywhere/credential/get.sh
@@ -1,19 +1,8 @@
-# 生成CA根证书
 openssl genrsa -out ca.key 2048
-# 使用-x509参数生成证书
-openssl req -new -x509 -days 7200 -key ca.key -out ca.pem
-
-# 生成服务端证书
-# openssl genrsa -out server.key 2048
-openssl ecparam -genkey -name secp384r1 -out server.key
-# 生成证书请求文件
-openssl req -new -key server.key -out server.csr
-# 基于CA签发服务端证书
-openssl x509 -req -sha256 -CA ca.pem -CAkey ca.key -CAcreateserial -days 3650 -in server.csr -out server.pem
-
-
-# 生成客户端证书
-openssl ecparam -genkey -name secp384r1 -out client.key
-openssl req -new -key client.key -out client.csr
-openssl x509 -req -sha256 -CA ca.pem -CAkey ca.key -CAcreateserial -days 3650 -in client.csr -out client.pem
-
+openssl req -x509 -new -nodes -key ca.key -days 10000 -out ca.crt -subj "/CN=we-as-ca"
+openssl genrsa -out server.key 2048
+openssl req -new -key server.key -out server.csr -subj "/CN=localhost"
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 365
+openssl genrsa -out client.key 2048
+openssl req -new -key client.key -out client.csr -subj "/CN=localhost"
+openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 365
